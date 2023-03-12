64d287887c6cfb80e184fadc7416eb7912f46782
clarencetinator7 <clarencejoseph.dimafelix@gmail.com>
2023-03-12 18:56:53 +0800

    Initial commit

---
+++ Assets/PlayerB.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerB : MonoBehaviour
+{
+    public float paddleSpeed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move paddle up or down on key press
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector2.up * paddleSpeed * Time.deltaTime);
+        } else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector2.down * paddleSpeed * Time.deltaTime);
+        }
+
+        // Keep paddle within screen bounds using clamp
+        float y = Mathf.Clamp(transform.position.y, -3.73f, 3.73f);
+        transform.position = new Vector2(transform.position.x, y);
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Pingpong2D
+ 
