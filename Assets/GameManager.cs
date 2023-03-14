9877ebd6c0d26c16ee56f966cf52f5da6c6bfc23
clarencetinator7 <clarencejoseph.dimafelix@gmail.com>
2023-03-14 21:27:29 +0800

    add ball respawn function

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -14,19 +14,27 @@ public class GameManager : MonoBehaviour
   [SerializeField] TextMeshProUGUI playerAScoreText;
   [SerializeField] TextMeshProUGUI playerBScoreText;
 
+  [SerializeField] GameObject ball;
+
   public void scoreA()
   {
     playerAScore++;
     playerAScoreText.text = playerAScore.ToString();
+    respawnBall();
   }
 
   public void scoreB()
   {
     playerBScore++;
     playerBScoreText.text = playerBScore.ToString();
+    respawnBall();
   }
 
   // TODO: Game Reset
+  public void respawnBall()
+  {
+    Instantiate(ball, new Vector3(0, 0, 0), Quaternion.identity);
+  }
 
 
 }
